27b14a92b5348190e49d2f0be6601e0e22ba60d3
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-07-05 16:47:29 +0300

    Files updated

--- tests/test_auth_page.py
+++ tests/test_auth_page.py
@@ -1,11 +1,13 @@
 from pages.auth_page import AuthPage
 import time
 
+
 def test_auth_page(selenium):
     page = AuthPage(selenium)
     page.enter_email("fonovagafonov@yandex.ru")
-    page.enter_pass("12345")
+    time.sleep(2)
+    page.enter_pass("123456")
+    time.sleep(2)
     page.btn_click()
 
-    # знак != или == будет зависеть от того, верные или неверные данные мы вводим
-    assert page.get_relative_link() != '/all_pets', "login error"
+    assert page.get_relative_link() == '/all_pets', "login error"
